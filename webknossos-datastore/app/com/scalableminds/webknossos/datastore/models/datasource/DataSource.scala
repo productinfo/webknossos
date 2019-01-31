18fe5cb583dbeb54dffde94512624386d00e6ba4
Youri K <youri.kaminsky@student.hpi.de>
2019-01-31 14:43:30 +0100

    [WIP] add filesize attribute to datasource #3665

--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/models/datasource/DataSource.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/models/datasource/DataSource.scala
@@ -12,7 +12,11 @@ package object datasource {
     implicit val dataSourceIdFormat: Format[DataSourceId] = Json.format[DataSourceId]
   }
 
-  case class GenericDataSource[+T <: DataLayerLike](id: DataSourceId, dataLayers: List[T], scale: Scale) extends GenericInboxDataSource[T] {
+  case class GenericDataSource[+T <: DataLayerLike](id: DataSourceId,
+                                                    dataLayers: List[T],
+                                                    scale: Scale,
+                                                    fileSize: Option[Long] = None)
+      extends GenericInboxDataSource[T] {
 
     val toUsable: Option[GenericDataSource[T]] = Some(this)
 
@@ -31,24 +35,26 @@ package object datasource {
 
   object GenericDataSource {
 
-    implicit def dataSourceFormat[T <: DataLayerLike](implicit fmt: Format[T]): Format[GenericDataSource[T]] = new Format[GenericDataSource[T]] {
+    implicit def dataSourceFormat[T <: DataLayerLike](implicit fmt: Format[T]): Format[GenericDataSource[T]] =
+      new Format[GenericDataSource[T]] {
 
-      def reads(json: JsValue): JsResult[GenericDataSource[T]] = {
-        for {
-          id <- (json \ "id").validate[DataSourceId]
-          dataLayers <- (json \ "dataLayers").validate[List[T]]
-          scale <- (json \ "scale").validate[Scale]
-        } yield {
-          GenericDataSource(id, dataLayers, scale)
-        }
+        def reads(json: JsValue): JsResult[GenericDataSource[T]] =
+          for {
+            id <- (json \ "id").validate[DataSourceId]
+            dataLayers <- (json \ "dataLayers").validate[List[T]]
+            scale <- (json \ "scale").validate[Scale]
+            fileSize <- (json \ "fileSize").validateOpt[Long]
+          } yield {
+            GenericDataSource(id, dataLayers, scale, fileSize)
+          }
+
+        def writes(ds: GenericDataSource[T]) = Json.obj(
+          "id" -> DataSourceId.dataSourceIdFormat.writes(ds.id),
+          "dataLayers" -> ds.dataLayers.map(Json.toJson(_)),
+          "scale" -> Scale.scaleWrites.writes(ds.scale),
+          "fileSize" -> ds.fileSize
+        )
       }
-
-      def writes(ds: GenericDataSource[T]) = Json.obj(
-        "id" -> DataSourceId.dataSourceIdFormat.writes(ds.id),
-        "dataLayers" -> ds.dataLayers.map(Json.toJson(_)),
-        "scale" -> Scale.scaleWrites.writes(ds.scale)
-      )
-    }
   }
 
   type DataSource = GenericDataSource[DataLayer]
--- webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/DataSourceImporter.scala
+++ webknossos-datastore/app/com/scalableminds/webknossos/datastore/services/DataSourceImporter.scala
@@ -11,6 +11,7 @@ import com.scalableminds.util.tools.ExtendedTypes._
 import org.apache.commons.io.FilenameUtils
 
 import scala.collection.mutable.ArrayBuffer
+import scala.sys.process.Process
 
 case class DataSourceImportReport[A](ctx: A, messages: ArrayBuffer[(String, String)] = ArrayBuffer.empty) {
 
@@ -27,20 +28,24 @@ trait DataSourceImporter {
 
   def dataFileExtension: String
 
-  protected def exploreLayer(name: String, baseDir: Path, previous: Option[DataLayer])(implicit report: DataSourceImportReport[Path]): Box[DataLayer]
+  protected def exploreLayer(name: String, baseDir: Path, previous: Option[DataLayer])(
+      implicit report: DataSourceImportReport[Path]): Box[DataLayer]
 
-  def exploreDataSource(id: DataSourceId, baseDir: Path, previous: Option[DataSource], report: DataSourceImportReport[Path]): Box[DataSource] = {
+  def exploreDataSource(id: DataSourceId,
+                        baseDir: Path,
+                        previous: Option[DataSource],
+                        report: DataSourceImportReport[Path]): Box[DataSource] =
     PathUtils.listDirectories(baseDir).map { layerDirs =>
       val layers = layerDirs.flatMap { layerDir =>
         val layerName = layerDir.getFileName.toString
         val previousLayer = previous.flatMap(_.getDataLayer(layerName))
         exploreLayer(layerName, layerDir, previousLayer)(report.withContext(_.resolve(layerName)))
       }
-      GenericDataSource(id, layers, previous.map(_.scale).getOrElse(Scale.default))
+      GenericDataSource(id, layers, previous.map(_.scale).getOrElse(Scale.default), getDataSourceFileSize(baseDir))
     }
-  }
 
-  protected def guessLayerCategory(layerName: String, elementClass: ElementClass.Value)(implicit report: DataSourceImportReport[Path]): Category.Value = {
+  protected def guessLayerCategory(layerName: String, elementClass: ElementClass.Value)(
+      implicit report: DataSourceImportReport[Path]): Category.Value = {
     val ColorRx = ".*color.*".r
     val MaskRx = ".*mask.*".r
     val SegmentationRx = ".*segmentation.*".r
@@ -59,34 +64,45 @@ trait DataSourceImporter {
     }
   }
 
-  protected def parseResolutionName(path: Path): Option[Either[Int, Point3D]] = {
+  protected def parseResolutionName(path: Path): Option[Either[Int, Point3D]] =
     path.getFileName.toString.toIntOpt match {
       case Some(resolutionInt) => Some(Left(resolutionInt))
       case None => {
         val pattern = """(\d+)-(\d+)-(\d+)""".r
         path.getFileName.toString match {
           case pattern(x, y, z) => Some(Right(Point3D(x.toInt, y.toInt, z.toInt)))
-          case _ => None
+          case _                => None
         }
       }
     }
-  }
 
   protected def resolutionDirFilter(path: Path): Boolean = parseResolutionName(path).isDefined
 
-  protected def resolutionDirSortingKey(path: Path) = {
+  protected def resolutionDirSortingKey(path: Path) =
     parseResolutionName(path).get match {
-      case Left(int) => int
+      case Left(int)    => int
       case Right(point) => point.maxDim
     }
-  }
 
-  protected def exploreMappings(baseDir: Path): Set[String] = {
-    PathUtils.listFiles(
-      baseDir.resolve(MappingProvider.mappingsDir),
-      PathUtils.fileExtensionFilter(MappingProvider.mappingFileExtension)).map {
-      paths => paths.map(path => FilenameUtils.removeExtension(path.getFileName.toString))
-    }.getOrElse(Nil).toSet
-  }
+  protected def exploreMappings(baseDir: Path): Set[String] =
+    PathUtils
+      .listFiles(baseDir.resolve(MappingProvider.mappingsDir),
+                 PathUtils.fileExtensionFilter(MappingProvider.mappingFileExtension))
+      .map { paths =>
+        paths.map(path => FilenameUtils.removeExtension(path.getFileName.toString))
+      }
+      .getOrElse(Nil)
+      .toSet
+
+  protected def getDataSourceFileSize(baseDir: Path): Option[Long] =
+    try {
+      val lineStream = Process(List("du", "-sk"), baseDir.toFile).lineStream
+      val returnString = lineStream.mkString
+      val index = returnString.indexOf('.')
+      val size = returnString.substring(0, index - 1)
+      Some(size.toLong * 1000)
+    } catch {
+      case _ => None
+    }
 
 }
